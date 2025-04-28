11c6d3010cc55bd22851a6b2f093cb13e58e1816
Moritz-Schule <210032@studierende.htl-donaustadt.at>
2025-04-28 12:04:07 +0200

    BYNDFM-39

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,47 +1,87 @@
-import {Component} from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonIcon} from '@ionic/angular/standalone';
-import {addIcons} from 'ionicons';
-import {searchOutline} from 'ionicons/icons';
+import {Component, OnInit} from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonItem,
+  IonInput,
+  IonIcon,
+  IonList,
+  IonLabel,
+  IonThumbnail,
+  IonSkeletonText,
+  IonSpinner
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { searchOutline, radioOutline } from 'ionicons/icons';
 import {RadioBrowserService} from "../services/radio-browser.service";
-import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
-
+import { FormsModule } from '@angular/forms';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonIcon, FormsModule, NgIf]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonItem,
+    IonInput,
+    IonIcon,
+    IonList,
+    IonLabel,
+    IonThumbnail,
+    IonSpinner,
+    FormsModule,
+    NgIf,
+    NgFor
+  ]
 })
-export class Tab2Page /*implements OnInit*/ {
+export class Tab2Page {
   searchTerm: string = '';
   searchResults: any[] = [];
-
+  isLoading: boolean = false;
 
   constructor(private radioBrowserService: RadioBrowserService) {
     addIcons({
-      searchOutline
+      searchOutline,
+      radioOutline
     });
   }
 
-  /*ngOnInit() {
-
-    this.radioBrowserService.getSearchResults('radio wien').subscribe(
-      results => {
-        console.log('Gefundene Sender:', results);
-      }
-    );
-  }*/
-
   onSearch() {
     if (this.searchTerm.trim()) {
+      this.isLoading = true;
       this.radioBrowserService.getSearchResults(this.searchTerm).subscribe(
         results => {
           this.searchResults = results;
+          this.isLoading = false;
           console.log('Gefundene Sender:', results);
+        },
+        error => {
+          console.error('Fehler bei der Suche:', error);
+          this.isLoading = false;
         }
       );
     }
+  }
 
+  formatTags(tags: string | string[]): string {
+    if (!tags) return '';
+
+    // Wenn tags ein String ist, splitte es an Kommas
+    if (typeof tags === 'string') {
+      return tags.split(',').map(tag => tag.trim()).join(', ');
+    }
+
+    // Wenn tags ein Array ist
+    if (Array.isArray(tags)) {
+      return tags.join(', ');
+    }
+
+    return '';
   }
 }
