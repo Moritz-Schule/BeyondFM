0afd772822e8af5d3c24a33ba16543856d18bac0
Moritz-Schule <210032@studierende.htl-donaustadt.at>
2025-05-27 13:27:18 +0200

    BYNDFM-100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
+import {IonApp, IonRouterOutlet} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
+  imports: [
+    IonApp,
+    IonRouterOutlet
+  ]
 })
 export class AppComponent {
   constructor(private platform: Platform) {
