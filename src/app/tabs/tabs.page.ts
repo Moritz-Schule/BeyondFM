a59a726a48d2bb11da83f90e103ebbcd33e49b41
Moritz-Schule <210032@studierende.htl-donaustadt.at>
2025-06-06 13:25:46 +0200

    BYNDFM-96

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,18 +1,25 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { homeOutline, searchOutline, libraryOutline } from 'ionicons/icons';
+import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  standalone: true,
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, AsyncPipe]
 })
 export class TabsPage {
-  public environmentInjector = inject(EnvironmentInjector);
+  selectedTab$ = new BehaviorSubject<string>('tab1');
 
   constructor() {
     addIcons({ homeOutline, searchOutline, libraryOutline });
+  }
+
+  ionTabsDidChange(event: { tab: string }) {
+    this.selectedTab$.next(event.tab);
   }
 }
